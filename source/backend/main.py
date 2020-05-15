50a665f71f08d2408c860da3e16608a7b89b0954
curl -L https://get.rvm.io | bash -s stable <roman.dvorovoy123@gmail.com>
2020-05-15 20:54:54 +0200

    core logic

--- source/backend/main.py
+++ source/backend/main.py
@@ -1,4 +1,63 @@
 import eel
 
-eel.init('../frontend')
-eel.start('index.html', port=8080, mode='electron', size=(256+64, 424+64))
+eel.init('../frontend/')
+
+@eel.expose
+def calcExp(exp):
+  """
+  1. prepare an expression for calculating,
+     - trim spaces
+     - replace math keyword with math functions (deg --> MATH_MODULE.degrees)
+  2. return error or calculated value as response
+  """
+
+  # normalize the expression
+  exp = exp.lower()
+
+  # replace this to that
+  for k, v in {
+    ' ':    '',
+    ',':    '.',
+
+    'sqrt': 'm.sqrt',
+
+    'deg':  'm.degrees',
+    'rad':  'm.radians',
+
+    'sin':  'm.sin',
+    'asin': 'm.asin',
+    'cos':  'm.cos',
+    'acos': 'm.acos',
+    'tan':  'm.tan',
+    'atan': 'm.atan'
+  }.items():
+    exp = exp.replace(k, v)
+
+  # try to evaluate the expression
+  import math as m
+
+  response = None
+
+  try:
+    response = {
+      'status': 'ok',
+      'details': eval(exp)
+    }
+
+  except Exception as e:
+    import sys
+
+    response = {
+      'status': 'error',
+      'details': str(sys.exc_info()[1])
+    }
+
+  finally:
+    import json
+
+    return json.dumps(response)
+
+eel.start('index.html', block=False)
+
+while True:
+  eel.sleep(5.0)
